ffe98a93609504070bcbea162312c9f5fa7f02fb
Rithvik086 <145756002+Rithvik086@users.noreply.github.com>
2025-09-15 21:22:31 +0530

    feat: implement authentication with JWT and update Prisma output path

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+import { signJwt, setAuthCookie } from "@/lib/auth";
+import { use } from "react";
+
+export async function POST(req: Request) {
+    try {
+        const { email, password } = await req.json();
+
+        const user = await prisma.user.findUnique({
+            where: { email },
+            include: { tenant: true }
+        })
+        if (!user) {
+            return NextResponse.json({ error: "Invalid email or password" }, { status: 401 });
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) {
+            return NextResponse.json({ error: "Invalid email or password" }, { status: 401 });
+        }
+
+        const payload = {
+            userId: user.id,
+            tenantId: user.tenantId,
+            role: user.role
+        }
+        const token = signJwt(payload);
+        await setAuthCookie(token);
+
+        return NextResponse.json({ message: "Login successful" });
+
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+}
---
+++ lib/auth.ts
@@ -0,0 +1,48 @@
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+
+const JWT_SECRET = process.env.JWT_SECRET || "fallbacksecret";
+
+
+//payload will be an object containing all reqd info
+
+export function signJwt(payload: object) {
+    return jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+}
+
+export function verifyJwt<T>(token: string): T | null {
+    try {
+        return jwt.verify(token, JWT_SECRET) as T;
+    } catch {
+        return null;
+    }
+}
+
+// Setup cookie
+export async function setAuthCookie(token: string) {
+    const cookieStore = await cookies();
+    cookieStore.set("auth", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        path: "/",
+        maxAge: 60 * 60, // 1 hour
+    });
+}
+
+// Clear auth cookie (async)
+export async function clearAuthCookie() {
+  const cookieStore = await cookies();
+  cookieStore.delete({
+    name: "auth",
+    path: "/",
+  });
+}
+
+// Get user from cookie (async)
+export async function getAuthUser<T>() {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("auth")?.value;
+    if (!token) return null;
+    return verifyJwt<T>(token);
+}
